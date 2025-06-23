9e8772a5d81194fb9ad9aeeed77f2e67e31399ce
Georg Lehner <jorge@at.anteris.net>
2025-06-23 13:29:18 +0200

    WIP: session authentication

--- session.tcl
+++ session.tcl
@@ -25,7 +25,7 @@ proc authn::session {Stage args} {
     }
     set URL [ns_conn url]
     if {$Location eq $URL} {
-	return filter_return; # Don't try to authenticate with session at the auth location
+	return filter_ok; # Don't try to authenticate with session at the auth location
     }
 
     set Session [getSession]
@@ -40,7 +40,7 @@ proc authn::session {Stage args} {
     }
     if {![nsv_dict exists authn session $Session username]} {
 	ns_returnbadrequest "Invalid session"
-	ns_log error "session has no user: $Session
+	ns_log error "session has no user: $Session"
 	return filter_return
     }
     if {[expired $Session]} {
@@ -63,16 +63,17 @@ proc auth::checkSession {} {
     # - If an expired session cookie is found delete the session, the session cookie and reauthenticate
 
     set Session [getSession]
-    if {[string length $Session] && [expired $Session]} {
+    set Form [ns_conn form]
+    set Action [ns_set get $Form action ""]
+    
+    if {[string length $Session] && ([expired $Session] || $Action in {renew logout})} {
 	destroySession $Session
 	# Note: Should we rather create a new empty ns_set?
 	set Headers [ns_conn outputheaders]
 	set SessionCookie [ns_config ns/server/[ns_info server]/authn/session cookie nsauthn_session]
 	set Path [ns_config ns/server/[ns_info server]/authn/session path /]
 	ns_set put $Headers "Set-Cookie" "$SessionCookie=;  Max-Age=0; path=$Path"
-	set Realm [ns_config ns/server/[ns_info server]/authn/session realm nsauthn_session]
-	ns_set put $Headers "WWW-Authenticate" "Basic realm=\"$Realm\", charset=\"UTF-8\""
-	ns_respond -status 401 -type text/plain -headers $Headers -string
+	reauthenticate $Headers
 	return filter_return
     }
     
@@ -82,14 +83,34 @@ proc auth::checkSession {} {
 proc authn::newSession {} {
     # request proc for auth location
     #
-    # - If no session cookie is present, or there is a valid one for
-    #   the currently authenticated user create or renew the session
-    #   and set a session cookie.
-    # - Otherwise signal the respective error
-    #
+    # Create a session and set the session cookie for the authenticated user.
+    # If there is already a session cookie set, destroy the session.
 
     set Auth [ns_conn auth]
-    
+    set Username [ns_set $Auth get username ""]
+    set Scheme [ns_set $Auth get AuthMethod ""]
+    set Headers [ns_conn outputheaders]
+
+    # Note: is $AuthMethod case sensitive?
+    if {![string length $Username] || $AuthMethod ne "Basic"} {
+	ns_log error "auth method incorrect or username empty: `$Scheme', `$Username'"
+	reauthenticate $Headers
+	return filter_return
+    }
+    set Session [getSession]
+    if {[string length $Session]} {
+	destroySession $Session
+    }
+    set Session [getNewSessionId]
+    nsv_dict set authn session $Session username $Username
+    set Expiry [ns_config ns/server/[ns_info server]/authn/session expiry 3600]
+    nsv_dict set authn session $Session expire [expr {[clock seconds]+$Seconds}]
+
+    set SessionCookie [ns_config ns/server/[ns_info server]/authn/session cookie nsauthn_session]
+    set Path [ns_config ns/server/[ns_info server]/authn/session path /]
+    # Note: if we use a _Host- prefix we cannot use a path other then /
+    ns_set put $Headers "Set-Cookie" "$SessionCookie=$Session; path=$Path; Secure; HttpOnly; SameSite=Strict"
+    ns_return 200 text/plain "Session for $Username successfully created: $Session"
 }
 
 proc authn::getSession {} {
@@ -113,19 +134,25 @@ proc authn::expired Session {
     return [expr {[nsv_dict get authn session $Session expiry] > [clock seconds]}]
 }
 
-proc authn:getNewSessionId {} {
+proc authn::getNewSessionId {} {
 
     set Count 6
     set Session ""
     while {[incr Count -1]} {
-	set NewSession [ns_rand 2147483647]
+	set NewSession [ns_md5 string [ns_rand 2147483647]]
 	if {![nsv_dict exists authn session $NewSession]} {
 	    set Session $NewSession
 	    break
 	}
     }
     if {![string length $Session]} {
-	error "not able to get unique session identifier"
+	error "unable to get unique session identifier"
     }
     return $Session
+}
+
+proc authn::reauthenticate Headers {
+    set Realm [ns_config ns/server/[ns_info server]/authn/session realm nsauthn_session]
+    ns_set put $Headers "WWW-Authenticate" "Basic realm=\"$Realm\", charset=\"UTF-8\""
+    ns_respond -status 401 -type text/plain -headers $Headers -string "Authenticate to get a valid session"
 }
